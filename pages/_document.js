a537615c5a30892a8eae4194d8b8ff9221e89ed5
Thomas <tfnielson.se@gmail.com>
2023-07-21 10:41:22 -0600

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html lang="en" className="font-raj">
-			<Head />
+			<Head >
+            <link rel="icon" href="/favicon.ico" sizes="any" />
+            </Head>
 
 			<body className="font-raj">
 				<Main />
