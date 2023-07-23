89dc6b606f72317a3e9b28d1637f98e727db5ec0
Thomas <tfnielson.se@gmail.com>
2023-07-23 07:44:23 -0600

    routes issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Projects from "./components/Projects";
 
 const router = createBrowserRouter([
-    {
-      path: "*",
-      element: <App />,
-    },
-  ]);
-  
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <RouterProvider router={router} />
-  );
+	{
+		path: "*",
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/about",
+				element: <About />,
+			},
+			{
+				path: "/projects",
+				element: <Projects />,
+			},
+		],
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<RouterProvider router={router} />
+);
