34ecea7de1971df504c3b47ce4e00c3145ac65a4
Thomas <tfnielson.se@gmail.com>
2023-07-23 06:48:31 -0600

    routes issue

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
-
 	return (
 		<section className={`flex flex-col`}>
 			<div className="flex-none">
@@ -22,11 +21,12 @@ function App() {
 			</div>
 			<main className={`min-h-screen grow`}>
 				<Routes>
-					<Route exact path="/" element={<Home />} />
-                    <Route exact path="/about" element={<About />} />
-                    <Route exact path="/projects" element={<Projects />} />
-                    <Route exact path="/services" element={<Services />} />
-                    <Route exact path="/contact" element={<Contact />} />
+					<Route exact path="/" element={<Home />}>
+						<Route exact path="about" element={<About />} />
+						<Route exact path="projects" element={<Projects />} />
+						<Route exact path="services" element={<Services />} />
+						<Route exact path="contact" element={<Contact />} />
+					</Route>
 				</Routes>
 			</main>
 			<div className="flex-none">
